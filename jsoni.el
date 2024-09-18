b5a8629bb0d0d70c7d269617f97988af3886ba0b
Prikaz98 <77636647+Prikaz98@users.noreply.github.com>
2024-09-18 12:42:14 +0200

    fix json-minimize

--- jsoni.el
+++ jsoni.el
@@ -1,27 +1,31 @@
 ;;; jsoni.el -*- lexical-binding: t; -*-
 ;;; Code:
-
+(require 'dash)
 (require 'text-util)
+
+(defun json--special-chars-replace ()
+  "Create list of pattern to replace of special json symbols.
+
+Returns list of pair pattern to replace."
+  (thread-last
+    (list "\"" "{" "}" "\\[" "]" "," ":")
+    (-map (lambda (char)
+            (let ((to-char (string-replace "\\" "" char)))
+              (list
+               (concat "\\( +\\)" char) to-char
+               (concat char "\\( +\\)") to-char
+               (concat "\n" char) to-char
+               (concat char "\n") to-char))))
+    (-flatten)
+    (-partition 2)))
 
 ;;;###autoload
 (defun jsoni-minimalize ()
   "Minimize json in whole buffer."
   (interactive)
   (thread-last
-    (list
-     "^\\( +\\)\"" "\""
-     "^\\( +\\)\\[" "\\["
-     "^\\( +\\)]" "]"
-     "{\n" "{"
-     "\\[\n" "["
-     "}\n" "}"
-     "]\n" "]"
-     "\,\n" "\,"
-     "\"\n" "\""
-     "\\( +\\):\\( +\\)" ":")
-    (-partition 2)
+    (json--special-chars-replace)
     (-map (apply-partially 'apply 'text-util-replace-in-whole-buffer))))
-
 
 (provide 'jsoni)
 ;;; scalai.el
