3023d2a1481b78f08c95c2cd3d13a82e798fb909
Prikaz98 <77636647+Prikaz98@users.noreply.github.com>
2024-10-09 18:47:42 +0200

    Create scalai-imports-test-cases.scala

---
+++ scalai-imports-test-cases.scala
@@ -0,0 +1,12 @@
+import com.twitter.util.Future
+import com.twitter.util.Return
+import com.twitter.util.Throw
+//==>
+import com.twitter.util.{Future, Return, Throw}
+//===================================================
+import com.twitter.util.Future
+import com.twitter.util.Return
+import com.twitter.util.Throw
+import com.twitter.util._
+//==>
+import com.twitter.util._
